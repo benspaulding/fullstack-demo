9c82e9c1874ec9fb49ab3a6b0c822ac02a0cd6f2
Ben Spaulding <ben.spaulding@ourtilt.com>
2023-08-22 10:13:51 -0600

    Iterate

--- fe/ReadMe.md
+++
@@ -1,3 +0,0 @@
-# Fullstack Project → Frontend
-
-This is a starter template for [Learn Next.js](https://nextjs.org/learn).
--- fe/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  distDir: "build",
-};
-
-export default nextConfig;
-
-// exports = module.exports = nextConfig;
